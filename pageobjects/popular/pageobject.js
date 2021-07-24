40279e57945be3c0e99b3867592ab7c743a82165
tammkirill <ktamm@codeitup.ru>
2021-07-24 20:50:53 +0300

    Added second test popular (cutted)

--- pageobjects/popular/pageobject.js
+++ pageobjects/popular/pageobject.js
@@ -15,6 +15,28 @@ class PopularPO
     //get header first level
     get header1() {return $('h1')}
 
+    //get snippets array
+    get snippetArr() {return $$('[data-tid="af76fbb3"]')}
+
+    //get snippet's pictures
+    get snippetsPictures() {return $$('[data-tid="d813cf42"]')}
+
+    //get button forward to list pages
+    get buttonForward() {return $('[data-tid="6e4f0353"]')}
+
+    //get snippets link
+    async getLink(document)
+    {
+        return document.$('[data-tid="6cf86878"]');
+    }
+
+    async getArrayChildA()
+    {
+        return document.$$('a');
+    }
+
+
+
 }
 
 module.exports = new PopularPO();
---
+++ step-definition/popular/popular-links.js
@@ -0,0 +1,59 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+const assert = require('assert');
+
+//** Check if link for snippets is correct*/
+
+const MainPage = require('../../commands/commands-films-today');
+
+const PageObjects = require('../../pageobjects/popular/pageobject');
+
+const Command = require('../../commands/commands-films-today');
+
+
+Given (/I am on the (.+) page$/, async (popular) => {
+    await MainPage.open(popular);
+});
+
+When (/^I can see film snippet$/, async () => {
+    
+    const snippetsArr = await PageObjects.snippetArr;
+
+    //check if all snippets are displayed
+    for (let i = 0; i < snippetsArr.length; i++)
+    {
+        if (!snippetsArr[i].isDisplayed())
+        {
+            assert.fail('Snippet {i} is not displayed'.replace('{i}',i));
+        }
+    }
+
+});
+
+Then (/^I should be able to go to the page of this film: (.+)$/, async (linkName) => {
+    
+    const snippetsArr = await PageObjects.snippetArr;
+
+    //get array of all links
+    let linksArr = await Command.smthArray(snippetsArr, PageObjects.getLink);
+
+    let regExp = /^(film|series)\/\d+\/$/;
+
+    for (let i = 0; i < linksArr.length; i++)
+    {
+        let linkItem = await linksArr[i];
+
+        await linkItem.click();
+
+        let stringURL = await browser.getUrl();
+
+        //Check if link have right form
+        if (!await Command.compareLinks(stringURL, linkName, regExp)){
+            assert.fail('Link {linkName} is incorrect'.replace('{linkName}', stringURL));
+        }
+
+        await browser.back();
+    }
+    
+    
+}); 
